46b1fd2be403637e093e4fd1629b8c3ed9056501
harryingles <harry.ingles@outlook.com>
2018-05-29 22:38:29 +0100

    release version

--- main.js
+++ main.js
@@ -3,18 +3,9 @@ const url = require('url')
 const path = require('path')
 const Config = require('electron-config')
 const config = new Config()
-const electronOauth2 = require('electron-oauth2');
 require("electron-debug")({ enabled: true})
 
 
-let authConfig = {
-    clientId: '588d74ab-0a60-4380-b854-04ec702a4e02',
-    clientSecret: "SECRET_HERE",
-    authorizationUrl: 'https://dev.fuelrats.com/authorize',
-    tokenUrl: 'https://dev.api.fuelrats.com/oauth2/token',
-    useBasicAuthorizationHeader: false,
-    redirectUri: 'https://localhost'
-};
 
 function createWindow() {
     win = new BrowserWindow({width:800, height:600})
@@ -26,38 +17,9 @@ function createWindow() {
 
 }
 
-function getTokenFromAuth(){
-    const windowParams = {
-        alwaysOnTop: true,
-        autoHideMenuBar: false,
-        webPreferences: {
-            nodeIntegration: false
-        }
-    }
-
-    const options = {
-        scope: 'rescue.read',
-        accessType: 'code'
-    };
-
-    const myApiOauth = electronOauth2(authConfig, windowParams);
-
-    myApiOauth.getAuthorizationCode(options)
-        .then(token => {
-            // use your token.access_token
-            config.set('token', token.access_token)
-            console.log("token created, woo!")
-            createWindow()
-
-        });
-}
 
 function startApp() {
-    if (!config.has('token')){
-        getTokenFromAuth()
-    } else {
-        createWindow()
-    }
+    createWindow()
 
 }
 
